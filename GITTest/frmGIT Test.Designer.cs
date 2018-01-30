f855d25fd50fece02131dd003efb108db18b09e9
maxjmorris <p15213515@my365.dmu.ac.uk>
2018-01-30 09:51:02 +0000

    Testing 1

--- GITTest/frmGIT Test.Designer.cs
+++ GITTest/frmGIT Test.Designer.cs
@@ -59,7 +59,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GIT Test";
             this.ResumeLayout(false);
             this.PerformLayout();
 
